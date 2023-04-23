be84ec85870ce1b803c7232f1729934121e488da
ni-xon <nixon.s.uch@gmail.com>
2023-04-23 23:35:38 +1000

    refactor: extract magic values

--- src/hooks/useRadio.ts
+++ src/hooks/useRadio.ts
@@ -1,18 +1,27 @@
 import { useState } from 'react'
 import { Stream } from '@/types/stream'
 
+const DEFAULT_VOLUME = 0.5
+const DEFAULT_STREAM_INDEX = 0
+const DEFAULT_PLAYING = false
+
 const initialStreams: Stream[] = [
   {
     title: 'lofi hip hop radio 📚 - beats to relax/study to',
     host: 'LofiGirl',
     url: 'https://www.youtube.com/watch?v=jfKfPfyJRdk&ab_channel=LofiGirl',
   },
+  {
+    title: 'synthwave radio 🌌 - beats to chill/game to',
+    host: 'LofiGirl',
+    url: 'https://www.youtube.com/watch?v=MVPTGNGiI-4&ab_channel=LofiGirl',
+  },
 ]
 
 export const useRadio = () => {
-  const [volume, setVolume] = useState<number>(0.5)
-  const [isPlaying, setIsPlaying] = useState<boolean>(false)
-  const [streamIndex, setStreamIndex] = useState<number>(0)
+  const [volume, setVolume] = useState<number>(DEFAULT_VOLUME)
+  const [isPlaying, setIsPlaying] = useState<boolean>(DEFAULT_PLAYING)
+  const [streamIndex, setStreamIndex] = useState<number>(DEFAULT_STREAM_INDEX)
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [streams, setStreams] = useState<Stream[]>(initialStreams)
 
@@ -21,11 +30,15 @@ export const useRadio = () => {
   }
 
   const nextStream = () => {
-    setStreamIndex(streamIndex + 1)
+    streamIndex === streams.length - 1
+      ? setStreamIndex(0)
+      : setStreamIndex(streamIndex + 1)
   }
 
   const prevStream = () => {
-    setStreamIndex(streamIndex - 1)
+    streamIndex === 0
+      ? setStreamIndex(streams.length - 1)
+      : setStreamIndex(streamIndex - 1)
   }
 
   const getCurrentStream = () => streams[streamIndex]
