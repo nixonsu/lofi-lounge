fc64ae1d65fa6fd3e9426a767bff6de5521ba72c
ni-xon <nixon.s.uch@gmail.com>
2023-04-24 23:06:24 +1000

    feat: complete Slider and styling

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,23 +1,34 @@
 import React, { ChangeEvent } from 'react'
 import styles from '@/styles/Slider.module.css'
 
-interface Slider {
+interface NeonSliderProps {
   value: number
   handleChange: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-const Slider = ({ value, handleChange }: Slider) => {
+const Slider = ({ value, handleChange }: NeonSliderProps) => {
+  const handleTickClick = (index: number) => {
+    const event = {
+      target: {
+        value: `${index / 10}`,
+      },
+    }
+    handleChange(event as ChangeEvent<HTMLInputElement>)
+  }
+
+  const ticks = Array.from({ length: 11 }, (_, i) => (
+    <div
+      key={i}
+      className={`${styles.neonTick} 
+      ${i < value * 10 ? styles.active : ''} 
+      ${i === 10 ? styles.invisible : ''}`}
+      onClick={() => handleTickClick(i)}
+    />
+  ))
+
   return (
     <div className={`${styles.neonSliderContainer}`}>
-      <input
-        type="range"
-        min="0"
-        max="1"
-        step="0.1"
-        value={value}
-        onChange={handleChange}
-        className={`${styles.neonSlider}`}
-      />
+      <div className={styles.neonTickContainer}>{ticks}</div>
     </div>
   )
 }
