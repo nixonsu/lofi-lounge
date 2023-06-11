b2a7b8d6fbdc7942966ee96f97b113df3ad760ff
ni-xon <nixon.s.uch@gmail.com>
2023-06-12 00:40:04 +1000

    feat: add react player and implement useRadio hook

--- client/src/components/Radio.tsx
+++ client/src/components/Radio.tsx
@@ -1,23 +1,56 @@
 import IconButton from './IconButton'
 import { ReactComponent as Play } from './icons/Play.svg'
+import { ReactComponent as Pause } from './icons/Pause.svg'
 import { ReactComponent as Next } from './icons/Next.svg'
 import { ReactComponent as Shuffle } from './icons/Shuffle.svg'
 import { ReactComponent as Playlist } from './icons/Playlist.svg'
 import Slider from './Slider'
+import { useRadio } from '../hooks/useRadio'
+import ReactPlayer from 'react-player'
 
 const Radio = () => {
+  const {
+    volume,
+    isPlaying,
+    togglePlay,
+    setVolume,
+    nextStream,
+    prevStream,
+    getCurrentStream,
+    shuffleStream,
+  } = useRadio()
+
+  const { title, url } = getCurrentStream()
+
   return (
     <div className="flex flex-col items-start w-1/4 text-white">
-      <p>Current stream title</p>
+      <p>{title}</p>
 
       <div className="flex items-center justify-center">
-        <IconButton icon={<Play />} />
-        <IconButton icon={<Next className="rotate-180" />} />
-        <IconButton icon={<Next />} />
-        <IconButton icon={<Shuffle />} />
-        <Slider value={5} />
+        {isPlaying ? (
+          <IconButton onClick={togglePlay} icon={<Pause />} />
+        ) : (
+          <IconButton onClick={togglePlay} icon={<Play />} />
+        )}
+        <IconButton
+          onClick={prevStream}
+          icon={<Next className="rotate-180" />}
+        />
+        <IconButton onClick={nextStream} icon={<Next />} />
+        <IconButton onClick={shuffleStream} icon={<Shuffle />} />
+        <Slider
+          value={volume}
+          handleChange={(e) => setVolume(parseFloat(e.target.value))}
+        />
         <IconButton icon={<Playlist />} />
       </div>
+
+      <ReactPlayer
+        className="hidden"
+        url={url}
+        playing={isPlaying}
+        volume={volume}
+      />
     </div>
   )
 }
--- client/src/components/Slider.tsx
+++ client/src/components/Slider.tsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 
 interface Props {
   value: number
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-const Slider = ({ value }: Props) => {
-  const [activeIndex, setActiveIndex] = useState(value)
+const Slider = ({ value, handleChange }: Props) => {
+  const [activeIndex, setActiveIndex] = useState(value * 10)
 
   const handleSlideClick = (index: number) => {
     setActiveIndex(index)
+    const event = {
+      target: {
+        value: `${index / 10}`,
+      },
+    }
+    handleChange(event as ChangeEvent<HTMLInputElement>)
   }
 
   return (
