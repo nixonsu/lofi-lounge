ff1a3aab855f6a338f1563d5c35a937870ddbaa5
ni-xon <nixon.s.uch@gmail.com>
2023-06-12 14:28:33 +1000

    refactor: remove boilerplate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,21 +4,6 @@ import Radio from './components/Radio'
 import UtilityBar from './components/UtilityBar'
 
 function App() {
-  const pages = [
-    <div>
-      <h2>Page 1</h2>
-      <p>This is the content of page 1.</p>
-    </div>,
-    <div>
-      <h2>Page 2</h2>
-      <p>This is the content of page 2.</p>
-    </div>,
-    <div>
-      <h2>Page 3</h2>
-      <p>This is the content of page 3.</p>
-    </div>,
-  ]
-
   return (
     <div className="font-primary p-4 h-screen w-screen">
       <div className="h-full w-full grid grid-cols-2 grid-rows-3">
