16f864a1e4c5806fa0df8e170d4b5f361aabe61d
ni-xon <nixon.s.uch@gmail.com>
2023-06-26 22:22:36 +1000

    fix: fix type

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -37,7 +37,7 @@ const useTimer = (initialSeconds: number) => {
   }
 
   useEffect(() => {
-    let interval: number
+    let interval: NodeJS.Timer
 
     if (isRunning) {
       interval = setInterval(() => {
