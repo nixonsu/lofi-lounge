9ee11f8cfaa125bb5f5027abe4ff05accc990547
ni-xon <nixon.s.uch@gmail.com>
2023-06-26 22:21:01 +1000

    build: install and configure vitest and react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
@@ -5,4 +6,8 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
 })
