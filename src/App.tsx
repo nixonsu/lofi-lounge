f4fbebd5a9bb61a04108c3cdb2986d2be5597dd4
ni-xon <nixon.s.uch@gmail.com>
2023-07-09 17:47:12 +1000

    feat: add suspense fallback placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { useRootStore } from '@store/rootStore'
 import TrackSelector from '@feature/TrackSelector'
 import SoundPlayerCollection from '@feature/SoundPlayerCollection'
 import FadeAnimation from '@components/animations/FadeAnimation'
-import { useEffect } from 'react'
+import { Suspense, useEffect } from 'react'
 import Timer from '@feature/Timer'
 import FadeAnimationHidden from '@components/animations/FadeAnimationHidden'
 import ThemeSelector from '@feature/ThemeSelector'
@@ -43,58 +43,62 @@ function App() {
       : document.body.requestFullscreen()
   }, [isFullscreen])
 
-  return sceneStore.scenes.length > 0 ? (
-    <div className="font-primary p-4 h-full w-full text-white">
-      <div className="h-full w-full grid grid-cols-2 grid-rows-2 tablet:grid-cols-1">
-        <div>
-          <Radio />
+  return (
+    <Suspense fallback={<Background className="bg-black" />}>
+      {sceneStore.scenes.length > 0 ? (
+        <div className="font-primary p-4 h-full w-full text-white">
+          <div className="h-full w-full grid grid-cols-2 grid-rows-2 tablet:grid-cols-1">
+            <div>
+              <Radio />
+            </div>
+
+            <div className="flex justify-end items-start tablet:hidden">
+              <TopIconBar />
+            </div>
+
+            <div className="flex flex-col-reverse items-start tablet:items-center gap-4">
+              <BottomIconBar />
+
+              <FadeAnimationHidden
+                isVisible={isSoundPlayerCollectionOpen}
+                className="h-1/2 absolute bottom-24"
+              >
+                <SoundPlayerCollection onClose={closeSoundPlayerCollection} />
+              </FadeAnimationHidden>
+
+              <FadeAnimationHidden
+                isVisible={isThemeSelectorOpen}
+                className="absolute bottom-24"
+              >
+                <ThemeSelector
+                  handleSelect={setTheme}
+                  currentTheme={theme}
+                  onClose={closeThemeSelector}
+                />
+              </FadeAnimationHidden>
+
+              <FadeAnimationHidden isVisible={isTimerOpen}>
+                <Timer onClose={closeTimer} />
+              </FadeAnimationHidden>
+            </div>
+          </div>
+
+          <FadeAnimation isVisible={isSceneSelectorOpen}>
+            <SceneSelector onClose={closeSceneSelector} />
+          </FadeAnimation>
+
+          <FadeAnimation isVisible={isTrackSelectorOpen}>
+            <TrackSelector onClose={closeTrackSelector} />
+          </FadeAnimation>
+
+          <Background className="-z-20" src={sceneStore.currentScene.src} />
+          <FadeAnimation isVisible={isBackgroundDim}>
+            <Background className="-z-10 bg-black bg-opacity-50" />
+          </FadeAnimation>
         </div>
-
-        <div className="flex justify-end items-start tablet:hidden">
-          <TopIconBar />
-        </div>
-
-        <div className="flex flex-col-reverse items-start tablet:items-center gap-4">
-          <BottomIconBar />
-
-          <FadeAnimationHidden
-            isVisible={isSoundPlayerCollectionOpen}
-            className="h-1/2 absolute bottom-24"
-          >
-            <SoundPlayerCollection onClose={closeSoundPlayerCollection} />
-          </FadeAnimationHidden>
-
-          <FadeAnimationHidden
-            isVisible={isThemeSelectorOpen}
-            className="absolute bottom-24"
-          >
-            <ThemeSelector
-              handleSelect={setTheme}
-              currentTheme={theme}
-              onClose={closeThemeSelector}
-            />
-          </FadeAnimationHidden>
-
-          <FadeAnimationHidden isVisible={isTimerOpen}>
-            <Timer onClose={closeTimer} />
-          </FadeAnimationHidden>
-        </div>
-      </div>
-
-      <FadeAnimation isVisible={isSceneSelectorOpen}>
-        <SceneSelector onClose={closeSceneSelector} />
-      </FadeAnimation>
-
-      <FadeAnimation isVisible={isTrackSelectorOpen}>
-        <TrackSelector onClose={closeTrackSelector} />
-      </FadeAnimation>
-
-      <Background className="-z-20" src={sceneStore.currentScene.src} />
-      <FadeAnimation isVisible={isBackgroundDim}>
-        <Background className="-z-10 bg-black bg-opacity-50" />
-      </FadeAnimation>
-    </div>
-  ) : null
+      ) : null}
+    </Suspense>
+  )
 }
 
 export default observer(App)
