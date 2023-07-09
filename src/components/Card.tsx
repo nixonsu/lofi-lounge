7c9e3b891ac47d7a575daa2a159fb24ee9d0932d
ni-xon <nixon.s.uch@gmail.com>
2023-07-09 17:35:02 +1000

    style: add image loading placeholder

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 interface Props {
   id: string
   src: string
@@ -7,6 +9,7 @@ interface Props {
 }
 
 const Card = ({ id, src, text, handleClick, isSelected }: Props) => {
+  const [isLoaded, setIsLoaded] = useState(false)
   return (
     <div
       className={`flex flex-col h-full w-full gap-4 items-center cursor-pointer rounded ${
@@ -16,7 +19,16 @@ const Card = ({ id, src, text, handleClick, isSelected }: Props) => {
         handleClick(id)
       }}
     >
-      <img className="h-5/6 w-full object-cover rounded" src={src} />
+      {isLoaded ? null : (
+        <div className="h-5/6 w-full rounded bg-black opacity-90" />
+      )}
+      <img
+        onLoad={() => setIsLoaded(true)}
+        className={`h-5/6 w-full object-cover rounded ${
+          isLoaded ? 'visible' : 'invisible'
+        }`}
+        src={src}
+      />
       <p className="text-center pl-4 pr-4 pb-4">{text}</p>
     </div>
   )
