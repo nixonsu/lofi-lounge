73b19df0ab9b02c05e5f671d4bbd14d8a0aea033
ni-xon <nixon.s.uch@gmail.com>
2023-07-09 17:21:22 +1000

    feat: merge top bar functionality to bottom bar on mobile

--- src/feature/BottomIconBar.tsx
+++ src/feature/BottomIconBar.tsx
@@ -5,6 +5,8 @@ import { ReactComponent as GalleryIcon } from 'pixelarticons/svg/image.svg'
 import { ReactComponent as MusicIcon } from 'pixelarticons/svg/music.svg'
 import { ReactComponent as ClockIcon } from 'pixelarticons/svg/clock.svg'
 import { ReactComponent as PaintIcon } from 'pixelarticons/svg/paint-bucket.svg'
+import { ReactComponent as MoonIcon } from '@components/icons/Moon.svg'
+import { ReactComponent as FullscreenIcon } from 'pixelarticons/svg/scale.svg'
 import IconBar from '@root/components/IconBar'
 
 interface Props {
@@ -20,6 +22,9 @@ const BottomIconBar = ({ className }: Props) => {
     toggleSoundPlayerCollection,
     toggleTimer,
     isTimerDone,
+    toggleIsBackgroundDim,
+    isBackgroundDim,
+    toggleFullscreen,
   } = uiStore
 
   return (
@@ -32,6 +37,14 @@ const BottomIconBar = ({ className }: Props) => {
         isRedGlow={isTimerDone}
       />
       <IconButton icon={<PaintIcon />} onClick={toggleThemeSelector} />
+      <div className="hidden tablet:visible tablet:flex tablet:gap-4">
+        <IconButton
+          icon={<MoonIcon />}
+          onClick={toggleIsBackgroundDim}
+          isEnabled={!isBackgroundDim}
+        />
+        <IconButton icon={<FullscreenIcon />} onClick={toggleFullscreen} />
+      </div>
     </IconBar>
   )
 }
